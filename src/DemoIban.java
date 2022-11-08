988b74bd9d6f152573d4c611d5a21ea3f65898f4
dxsattler <daniel.sattler2@igt.com>
2022-11-08 19:28:03 +0100

    Last Changes

--- src/DemoIban.java
+++ src/DemoIban.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class DemoIban {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DemoIban {
+
+    public static void main(String[] args) {
+
+
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Eingabe von IBAN:");
+
+        String ibanEntry = myObj.nextLine();  // Read user input
+
+        IbanValidationService ibanValidationService = new IbanValidationService(ibanEntry);
+
+        System.out.println("\nLändercode: " + ibanValidationService.getCountrycode() + " (" + ibanValidationService.getCountryCodeNumber() + ")");
+        System.out.println("Prüfsumme: " + ibanValidationService.getCheckSum());
+        System.out.println("Kontonummer: " + ibanValidationService.getAccountNumber());
+        System.out.println("Prüfzahl: " + ibanValidationService.getFullCode());
+
+        int[] checkNumberAsInt = ibanValidationService.buildArray();
+        System.out.println("Prüfzahl als Zahl: " + Arrays.toString(checkNumberAsInt));
+
+        System.out.print("Ist dieser IBAN korrekt? ");
+        if(ibanValidationService.isIbanCorrect())
+            System.out.print("Ja");
+        else
+            System.out.print("Nein");
+        System.out.println(" (" + ibanValidationService.calcCheckSum() + ")");
+
+    }
+
 }
--- src/IbanValidationService.java
+++ src/IbanValidationService.java
@@ -1,2 +1,66 @@
-package PACKAGE_NAME;public class IbanValidationService {
+public class IbanValidationService {
+    private String iBan;
+    private int calculatedCheckSum;
+
+    public IbanValidationService(String iBan) {
+        this.iBan = iBan;
+        this.calculatedCheckSum = -1;
+    }
+
+    public String getCountrycode(){
+        return iBan.substring(0,2);
+    }
+
+    public int getCheckSum(){
+        String part = iBan.substring(2,4);
+        this.calculatedCheckSum = Integer.valueOf(part);
+        return this.calculatedCheckSum;
+    }
+
+    public String getAccountNumber(){
+        return iBan.substring(4,iBan.length());
+    }
+
+    public String getCountryCodeNumber(){
+
+        char checkNumber = '7';
+
+        int intA = iBan.charAt(0) - checkNumber;
+        int intB = iBan.charAt(1) - checkNumber;
+        String countryCodeNumber = "" + intA + intB * 100;
+        return countryCodeNumber;
+    }
+
+    public String getFullCode(){
+        return getAccountNumber() + getCountryCodeNumber();
+    }
+
+    public int[] buildArray(){
+        int[] fullCode = new int[getFullCode().length()];
+
+        for (int i = 0; i < getFullCode().length(); i++) {
+            fullCode[i] = getFullCode().charAt(i) - '0';
+        }
+
+        return fullCode;
+    }
+
+    public int calcCheckSum(){
+
+        int checkNumber = 0;
+        int sum = 0;
+
+        for (int i = 0; i < getFullCode().length(); i++) {
+
+            checkNumber = ((checkNumber * 10) + buildArray()[i]) % 97;
+
+
+        }
+        checkNumber = 98 - (checkNumber % 97);
+        return checkNumber;
+    }
+
+    public boolean isIbanCorrect(){
+        return calcCheckSum() == getCheckSum();
+    }
 }
